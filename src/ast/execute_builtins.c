cbd03b5e58bd60210d025e9fb173a084736f137c
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-04-15 11:17:51 +0200

    feat: remove is_unimplemented_builtin

--- src/ast/execute_builtins.c
+++ src/ast/execute_builtins.c
@@ -8,26 +8,6 @@
 #include "mysh.h"
 
 /**
- * @brief Checks if a command is a builtin that is recognized but not
- * implemented.
- *
- * @param cmd : The command name to check.
- * @return : 1 if it's an unimplemented builtin, 0 otherwise.
- */
-static int is_unimplemented_builtin(char *cmd)
-{
-    const char *unimplemented_builtins[] = {
-        "history", "pushd", "popd", "dirs", "set", NULL
-    };
-
-    for (int i = 0; unimplemented_builtins[i]; i++) {
-        if (strcmp(cmd, unimplemented_builtins[i]) == 0)
-            return 1;
-    }
-    return 0;
-}
-
-/**
  * @brief Handles execution of MOST builtin commands (all except exit).
  *
  * @param args : Arguments of the command
@@ -71,7 +51,5 @@ int handle_builtin_command(ast_node_t *node, int old_stdin, int old_stdout)
             restore_redirections(old_stdin, old_stdout);
         exit(status);
     }
-    if (is_unimplemented_builtin(node->args[0]))
-        return 0;
     return -1;
 }
